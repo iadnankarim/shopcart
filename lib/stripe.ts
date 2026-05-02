6226f2a719e329547e9300273664eeeb41f557d3
iadnankarim <adnankarim725@gmail.com>
2026-05-02 19:17:53 +0500

    fix: remove top-level throw in stripe.ts to prevent build crash

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,10 +1,6 @@
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("STRIPE_SCERET_KEY is not set");
-}
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
   apiVersion: "2025-01-27.acacia",
 });
 
